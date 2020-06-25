66334ee6a41d75a296f081961c0dbf47d2cf361c
Anmol Kalra <42032383+anmolkalra17@users.noreply.github.com>
2020-06-25 16:32:06 +0530

    Create q5.py

---
+++ q5.py
@@ -0,0 +1,20 @@
+test_cases = int(input("Enter number of test cases: \n"))
+
+def string_div(string1, string2):
+    string1_1 = string1[:len(string1)//2]
+    string1_2 = string1[len(string1)//2:]
+
+    string2_1 = string2[:len(string2) // 2]
+    string2_2 = string2[len(string2) // 2:]
+
+    if string1_1 == string2_1 and string1_2 == string2_2 or string1_1 == string2_2 and string1_2 == string1_1:
+        print("YES")
+    else:
+        print("NO")
+
+
+if 1 <= test_cases <= 10:
+    for i in range(0, test_cases):
+        s1 = input()
+        s2 = input()
+        string_div(s1, s2)
