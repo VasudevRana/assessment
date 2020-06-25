9d147a422a78fce989ed00451af708e4fee7d980
Anmol Kalra <42032383+anmolkalra17@users.noreply.github.com>
2020-06-25 16:49:26 +0530

    final

--- q5.py
+++ q5.py
@@ -8,16 +8,17 @@ def string_div(string1, string2):
     string2_2 = string2[len(string2) // 2:]
 
     if string1_1 == string2_1 and string1_2 == string2_2 or string1_1 == string2_2 and string1_2 == string2_1:
-        results.append("YES \n")
+        results.append("YES")
     else:
-        results.append("NO \n")
+        results.append("NO")
 
 
 if 1 <= test_cases <= 10:
     for i in range(0, test_cases):
         s1 = input()
         s2 = input()
-        string_div(s1, s2)
+        if 1 <= len(s1) <= 50000 and 1 <= len(s2) <= 50000:
+            string_div(s1, s2)
 
-print(*results, sep="")
+print(*results, sep="\n")
 #end
