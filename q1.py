7c07224e2d9acfc3a694ca5931401ebf4e2e431c
Anmol Kalra <42032383+anmolkalra17@users.noreply.github.com>
2020-06-25 19:53:10 +0530

    final

--- q1.py
+++ q1.py
@@ -1,37 +1,37 @@
-coins = []
-n = int(input())
-score_1 = 0
-score_2 = 0
-empty = []
+if __name__ == '__main__':
+    coins = []
+    n = int(input())
+    score_1 = 0
+    score_2 = 0
+    empty = []
 
-def main():
-    if 1 < n <= 30 and n % 2 != 0:
-        exit()
+    def main():
+        if 1 < n <= 30 and n % 2 != 0:
+            exit()
 
-    for i in range(1, n+1):
-        coins.append(i)
+        for i in range(1, n+1):
+            coins.append(i)
 
-    print(coins)
+        print(coins)
 
-def p1_selectable():
-    global coins
-    global score_1
-    select = max(coins)
-    score_1 += select
-    coins.remove(select)
+    def p1_selectable():
+        global coins
+        global score_1
+        select = max(coins)
+        score_1 += select
+        coins.remove(select)
 
-def p2_selectable():
-    global coins
-    global score_2
-    select = max(coins)
-    score_2 += select
-    coins.remove(select)
+    def p2_selectable():
+        global coins
+        global score_2
+        select = max(coins)
+        score_2 += select
+        coins.remove(select)
 
 
-main()
-while coins != empty:
-    p1_selectable()
-    p2_selectable()
+    main()
+    while coins != empty:
+        p1_selectable()
+        p2_selectable()
 
-print(score_1)
-#end
+    print(score_1)
