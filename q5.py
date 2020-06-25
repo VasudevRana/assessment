71d9c7f92519df3903204926e6a15925ada2dd02
Anmol Kalra <42032383+anmolkalra17@users.noreply.github.com>
2020-06-25 16:42:20 +0530

    final

--- q5.py
+++ q5.py
@@ -6,8 +6,9 @@ def string_div(string1, string2):
 
     string2_1 = string2[:len(string2) // 2]
     string2_2 = string2[len(string2) // 2:]
+    print(string1_1, string1_2, string2_1, string2_2)
 
-    if string1_1 == string2_1 and string1_2 == string2_2 or string1_1 == string2_2 and string1_2 == string1_1:
+    if string1_1 == string2_1 and string1_2 == string2_2 or string1_1 == string2_2 and string1_2 == string2_1:
         print("YES")
     else:
         print("NO")
