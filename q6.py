5c2400ba09182398c6bf71281b45751d7592cbc2
Anmol Kalra <42032383+anmolkalra17@users.noreply.github.com>
2020-06-26 17:05:02 +0530

    Create q6.py

---
+++ q6.py
@@ -0,0 +1,21 @@
+def countFriendsPairings(n):
+    dp = [0 for i in range(n + 1)]
+    for i in range(n + 1):
+
+        if i <= 2:
+            dp[i] = i
+        else:
+            dp[i] = dp[i - 1] + (i - 1) * dp[i - 2]
+
+    return dp[n]
+
+
+if __name__ == '__main__':
+    t = int(input("Enter number of test cases\n"))
+
+    while t > 0:
+        n = int(input("Enter number of friends\n"))
+        if 1 <= n < 30:
+            print(countFriendsPairings(n))
+        t -= 1
+# end
