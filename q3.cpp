4dad099bac49ebe082cef2b370cff582a27c8fa6
Anmol Kalra <42032383+anmolkalra17@users.noreply.github.com>
2020-06-26 19:37:43 +0530

    need tweaks

--- q3.cpp
+++ q3.cpp
@@ -1,88 +1,46 @@
-#include <stdio.h>  
-#define N 4 
+N = int(input())
+M = int(input())
 
-bool solveMazeUtil( 
-	int maze[N][N], int x, 
-	int y, int sol[N][N]); 
+def printSolution(sol):
+    for i in sol:
+        for j in i:
+            print(str(j) + " ", end="")
+        print("")
 
-void printSolution(int sol[N][N]) 
-{ 
-	for (int i = 0; i < N; i++) { 
-		for (int j = 0; j < N; j++) 
-			printf(" %d ", sol[i][j]); 
-		printf("\n"); 
-	} 
-} 
+def isSafe(maze, x, y):
+    if 0 <= x < N and 0 <= y < N and maze[x][y] == 1:
+        return True
+    return False
 
-bool isSafe(int maze[N][N], int x, int y) 
-{ 
-	// if (x, y outside maze) return false 
-	if ( 
-		x >= 0 && x < N && y >= 0 
-		&& y < N && maze[x][y] == 1) 
-		return true; 
+def solveMaze(maze):
+    sol = [[0 for j in range(4)] for i in range(4)]
 
-	return false; 
-} 
+    if not solveMazeUtil(maze, 0, 0, sol):
+        print("Solution doesn't exist")
+        return False
+    printSolution(sol)
+    return True
 
-bool solveMaze(int maze[N][N]) 
-{ 
-	int sol[N][N] = { { 0, 0, 0, 0 }, 
-					{ 0, 0, 0, 0 }, 
-					{ 0, 0, 0, 0 }, 
-					{ 0, 0, 0, 0 } }; 
+def solveMazeUtil(maze, x, y, sol):
+    if x == N - 1 and y == N - 1 and maze[x][y] == 1:
+        sol[x][y] = 1
+        return True
+    if isSafe(maze, x, y):
+        sol[x][y] = 1
+        if solveMazeUtil(maze, x + 1, y, sol):
+            return True
 
-	if (solveMazeUtil( 
-			maze, 0, 0, sol) 
-		== false) { 
-		printf("Solution doesn't exist"); 
-		return false; 
-	} 
+        if solveMazeUtil(maze, x, y + 1, sol):
+            return True
 
-	printSolution(sol); 
-	return true; 
-} 
+        sol[x][y] = 0
+        return False
 
-bool solveMazeUtil( 
-	int maze[N][N], int x, 
-	int y, int sol[N][N]) 
-{ 
-	if ( 
-		x == N - 1 && y == N - 1 
-		&& maze[x][y] == 1) { 
-		sol[x][y] = 1; 
-		return true; 
-	} 
 
-	if (isSafe(maze, x, y) == true) { 
-		// mark x, y as part of solution path 
-		sol[x][y] = 1; 
+if __name__ == "__main__":
+    maze = [[1, 0, 0, 0],
+            [1, 1, 0, 1],
+            [0, 1, 0, 0],
+            [1, 1, 1, 1]]
 
-		if (solveMazeUtil( 
-				maze, x + 1, y, sol) 
-			== true) 
-			return true; 
-
-		if (solveMazeUtil( 
-				maze, x, y + 1, sol) 
-			== true) 
-			return true; 
-
-		sol[x][y] = 0; 
-		return false; 
-	} 
-
-	return false; 
-} 
- 
-int main() 
-{ 
-	int maze[N][N] = { { 1, 0, 0, 0 }, 
-					{ 1, 1, 0, 1 }, 
-					{ 0, 1, 0, 0 }, 
-					{ 1, 1, 1, 1 } }; 
-
-	solveMaze(maze); 
-	return 0; 
-} 
-// not final
+    solveMaze(maze)
